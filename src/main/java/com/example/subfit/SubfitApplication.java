e42245921cbcaa6529c5f9b71167b1b8b527d388
westjuuu <westjiuuu@naver.com>
2024-09-27 18:00:28 +0900

    first commit

---
+++ src/main/java/com/example/subfit/SubfitApplication.java
@@ -0,0 +1,13 @@
+package com.example.subfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubfitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubfitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/subfit/SubfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.subfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
